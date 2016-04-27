5ee65bad24d6715af78f3f6d4d9afbe4db98cbb1
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-04-28 01:13:30 +0200

    Importing components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,12 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, RouterOutlet} from 'angular2/router';
+import {LoginComponent} from './user/login.component';
+import {RegistrationComponent} from './user/registration.component';
+import {HeaderComponent} from './common/header.component';
+import {AlbumsComponent} from './music/albums.component';
+import {AlbumComponent} from './music/album.component';
+import {WelcomeComponent} from './common/welcome.component';
+import {FooterComponent} from './common/footer.component';
 
 @Component({
     selector : 'my-app',
