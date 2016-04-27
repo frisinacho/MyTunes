7798307c90a0f4be2e27d09126a89f686169a262
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-04-27 13:00:21 +0200

    Basic AppComponent

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, RouterOutlet} from 'angular2/router';
+
+@Component({
+    selector : 'my-app',
+    template : ``,
+    directives : [RouterOutlet]
+})
+export class AppComponent {
+
+    constructor() {}
+}
