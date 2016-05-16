cc13dbbfca4e075afe3f288a1c5205f351b4ca71
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-05-17 01:33:50 +0200

    Creating User model object

--- app/user/models/user.ts
+++ app/user/models/user.ts
@@ -1,3 +1,19 @@
-/**
- * Created by Nacho on 17/5/16.
- */
+import {IUser} from './iuser';
+
+export class User implements IUser {
+
+    userName:string;
+    email:string;
+    password:string;
+    country:string;
+    birthday:Date;
+
+    /*
+     constructor(public userName : string, public email : string, public password : string, public country? : string, public birthday? :Date) {
+     }
+     */
+    constructor(authData:any) {
+        this.email = authData.password.email;
+    }
+
+}
