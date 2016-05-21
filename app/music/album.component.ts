d476c9fc99f29eace365019880a3727c9ec7d078
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-05-21 12:27:47 +0200

    Updating music components with full templates

--- app/music/album.component.ts
+++ app/music/album.component.ts
@@ -1,13 +1,47 @@
 import {Component} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+import {MusicService} from './services/music.srv';
+import {IAlbum, AlbumImageSize} from './models/ialbum';
 
 @Component({
-    selector : 'album-component',
+    selector : 'album',
     template : `
-        <div>
-            Album
+        <div *ngIf="album">
+            <div class="page-header">
+                <h1>{{ album.name }} <small>{{ album.artist }}</small></h1>
+            </div>
+            <div class="media">
+                  <div class="media-left">
+                    <a href="#">
+                      <img class="media-object" [src]="album.getImage(imageSize)">
+                    </a>
+                  </div>
+                  <div class="media-body">
+                    <ul>
+                        <li *ngFor="#song of album.songs">
+                            {{ song.name }}
+                        </li>
+                    </ul>
+                  </div>
+            </div>
         </div>
-    `
+    `,
+    providers : [MusicService]
 })
 export class AlbumComponent {
 
+    public album:IAlbum;
+    private imageSize:AlbumImageSize = AlbumImageSize.LARGE;
+
+    constructor(private routeParams:RouteParams, private musicService:MusicService) {
+        this.getAlbumInfo(routeParams.get("id"));
+    }
+
+    getAlbumInfo(id:string) {
+        this.musicService.albumInfo(id)
+            .subscribe(album => {
+                this.album = album;
+                var image = this.album.getImage(AlbumImageSize.LARGE);
+            });
+    }
 }
--- app/music/albums.component.ts
+++ app/music/albums.component.ts
@@ -1,13 +1,57 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit, Input} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+import {ROUTER_DIRECTIVES, RouteParams} from 'angular2/router';
+import {IAlbum, AlbumImageSize} from './models/ialbum';
+import {MusicService} from './services/music.srv';
+import {AlbumComponent} from './album.component';
+import {EllipsisPipe} from '../common/pipes/ellipsis.pipe';
+import {HightlightDirective} from '../common/directives/highlight.directive';
+import {PaginationComponent} from '../common/pagination.component';
+import {IPager} from '../common/models/ipager';
 
 @Component({
     selector : 'albums-component',
     template : `
-        <div>
-            Albums
-        </div>
-    `
+        <ul class="media-list">
+            <li class="media" *ngFor="#album of albums" [routerLink]="['Album', { id : album.id }]" appHighlight [hoverColor]="'whitesmoke'" [activeColor]="'gray'">
+                <div class="media-left">
+                    <a>
+                        <img class="media-object" [src]="album.getImage(albumImageSize)">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">{{ album.name | ellipsis:40}}</h4>
+                    <p>{{ album.artist }}</p>
+                </div>
+            </li>
+        </ul>
+        <pagination-component [pager]="pager" [pathName]="'Albums'"></pagination-component>
+    `,
+    providers  : [MusicService],
+    directives : [ROUTER_DIRECTIVES, HightlightDirective, PaginationComponent],
+    pipes      : [EllipsisPipe]
 })
-export class AlbumsComponent {
+export class AlbumsComponent implements OnInit {
 
+    public albums:Array<IAlbum> = [];
+    public albumImageSize:AlbumImageSize = AlbumImageSize.MEDIUM;
+    public pager:IPager;
+
+    constructor(private musicService : MusicService, private routeParams:RouteParams) {}
+
+    ngOnInit() {
+        this.albumsSearch(this.routeParams.get("query"), this.routeParams.get("page"));
+    }
+
+    albumsSearch(query:string, page:any) {
+        if (query) {
+            this.musicService.albumsSearch(query, parseInt(page))
+                .subscribe(res => {
+                    this.albums = res.albums;
+                    this.pager = res.pager;
+                });
+        } else {
+            this.albums = [];
+        }
+    }
 }
