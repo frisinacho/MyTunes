8038a02a9f50ae1319a38dded797d509a4e7d502
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-05-21 12:34:00 +0200

    Updating user components with full templates

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -1,13 +1,42 @@
 import {Component} from 'angular2/core';
+import {Router} from 'angular2/router';
+import {UserService} from './services/user.srv';
 
 @Component({
     selector : 'login-component',
     template : `
-        <div>
-            Login
-        </div>
-    `
+        <form #loginForm="ngForm">
+            <div class="alert alert-danger" role="alert" *ngIf="error">{{ error }}</div>
+            <div class="form-group">
+                <label for="login">Login</label>
+                <input type="text" placeholder="Login" class="form-control" required ngControl="userName" #userName="ngForm"/>
+                <div [hidden]="userName.valid || userName.pristine" class="alert alert-danger">Login required</div>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" placeholder="Password" class="form-control" required ngControl="password" #password="ngForm"/>
+                <div [hidden]="password.valid || password.pristine" class="alert alert-danger">Password required</div>
+            </div>
+            <button type="submit" class="btn btn-default" (click)="login(userName.value, password.value)" [disabled]="!loginForm.form.valid">Login</button>
+        </form>
+    `,
+    providers : [UserService]
 })
 export class LoginComponent {
 
+    public error : string;
+
+    constructor(private userService:UserService, private router:Router) {
+
+    }
+
+    public login(userName, password) {
+        this.userService.login(userName, password)
+            .subscribe(result => {
+                this.router.navigateByUrl("/");
+            }, error => {
+                this.error = error;
+            });
+
+    }
 }
--- app/user/registration.component.ts
+++ app/user/registration.component.ts
@@ -1,13 +1,60 @@
 import {Component} from 'angular2/core';
+import {Router} from 'angular2/router';
+import {UserService} from './services/user.srv';
+import {CountryService} from '../common/services/country.srv';
+import {IUser} from 'models/iuser';
+import {ICountry} from '../common/models/icountry';
 
 @Component({
     selector : 'registration-component',
     template : `
-        <div>
-            Registration
-        </div>
-    `
+        <form #registrationForm="ngForm">
+            <div class="form-group">
+                <input type="text" required placeholder="username" ngControl="userName" #userName="ngForm"/>
+                <div class="alert alert-danger" [hidden]="userName.valid || userName.pristine">Username required</div>
+            </div>
+            <div class="form-group">
+                <input type="text" required placeholder="email" ngControl="email" #email="ngForm"/>
+                <div class="alert alert-danger" [hidden]="email.valid || email.pristine">Email required</div>
+            </div>
+            <div class="form-group">
+                <input type="password" required placeholder="password" ngControl="password"  #password="ngForm"/>
+                <div class="alert alert-danger" [hidden]="password.valid || password.pristine">Password required</div>
+            </div>
+            <div class="form-group">
+                <select class="form-control" ngControl="country" #country="ngForm">
+                    <option value="">Select a country</option>
+                    <option *ngFor="#c of countries" [value]="c.alpha2Code">{{ c.name }}</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <input type="date" ngControl="birthday" #birthday="ngForm" />
+            </div>
+            <button type="submit" class="btn btn-default"
+                [disabled]="!registrationForm.valid"
+                (click)="register(userName.value, email.value, password.value, country.value, birthday.value)">
+                    Register
+            </button>
+        </form>
+    `,
+    providers : [UserService, CountryService]
 })
 export class RegistrationComponent {
 
+    public countries:Array<ICountry> = [];
+
+    constructor(private userService:UserService, private countryService:CountryService, private router:Router) {
+        this.countryService.getCountries()
+            .subscribe(countries => {
+                this.countries = countries;
+            })
+    }
+
+    register(username:string, email:string, password:string, country?:string, birthday?:Date) {
+        this.userService.register(username, email, password, country, birthday)
+            .subscribe(user => {
+                this.router.navigateByUrl("/");
+            });
+
+    }
 }
